065cd31dfd760aaa0cd8a31f22846f625e93f32d
Joanna Qian <honggzb@users.noreply.github.com>
2023-12-22 01:37:22 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Watch from "./components/watch";
 import Shoe from "./components/shoe";
+import Basic from "./components/basic";
+import RTexture from "./components/texture";
+import Room from "./components/room-dome";
 import './App.css';
 
 export default function App() {
@@ -11,13 +14,19 @@ export default function App() {
         <nav>
           <ul>
             <li> <Link to="/">Home</Link> </li>
+            <li> <Link to="/basic">Basic</Link> </li>
+            <li> <Link to="/rtexxture">RenderTexture</Link> </li>
             <li> <Link to="/shoe">Shoe</Link> </li>
             <li> <Link to="/watch">Watch</Link> </li>
+            <li> <Link to="/room">Room</Link> </li>
           </ul>
         </nav>
         <Routes>
           <Route path="/shoe" element={<Shoe/>}></Route>
           <Route path="/watch" element={<Watch />}></Route>
+          <Route path="/basic" element={<Basic/>}></Route>
+          <Route path="/rtexxture" element={<RTexture/>}></Route>
+          <Route path="/room" element={<Room/>}></Route>
           <Route path="/" element={<Home />}></Route>
         </Routes>
       </div>
